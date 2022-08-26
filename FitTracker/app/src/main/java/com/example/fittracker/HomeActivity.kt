250bf6350b8404c3cf33eab9191ff51ff1eb91d9
S1092514 <s1092514@studenti.univpm.it>
2022-08-26 18:30:25 +0200

    cambio look + aggiunta bottom navigation

--- FitTracker/app/src/main/java/com/example/fittracker/HomeActivity.kt
+++ FitTracker/app/src/main/java/com/example/fittracker/HomeActivity.kt
@@ -1,8 +1,14 @@
 package com.example.fittracker
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     private val diarioFragment = com.example.fittracker.diarioFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
+    }
 }
