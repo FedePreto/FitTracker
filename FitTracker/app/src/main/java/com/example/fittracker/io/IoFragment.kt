d7087eade5ef46fc9cdf6d4d04065f4b1e354bf7
S1092514 <s1092514@studenti.univpm.it>
2022-10-19 19:00:46 +0200

    inizio home

--- FitTracker/app/src/main/java/com/example/fittracker/io/IoFragment.kt
+++ FitTracker/app/src/main/java/com/example/fittracker/io/IoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fittracker.io
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,19 +8,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fittracker.R
+import com.example.fittracker.databinding.FragmentIoBinding
 
 class IoFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = IoFragment()
-    }
-
     private lateinit var viewModel: IoViewModel
+    private lateinit var binding: FragmentIoBinding
+    private var calorie_giornaliere = 2000
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding.progressCalorie.apply {
+            progressMax = calorie_giornaliere.toFloat()
+            setProgressWithAnimation(50f, 1000)
+            progressBarWidth = 5f
+            backgroundProgressBarWidth = 7f
+            progressBarColor = Color.GREEN
+        }
+
         return inflater.inflate(R.layout.fragment_io, container, false)
     }
 
