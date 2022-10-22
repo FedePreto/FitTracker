d1a1cceaa11115f2b446b701698b816efc103327
S1092514 <s1092514@studenti.univpm.it>
2022-10-22 15:02:37 +0200

    aggiungiLayout

--- FitTracker/app/src/main/java/com/example/fittracker/AggiungiActivity.kt
+++ FitTracker/app/src/main/java/com/example/fittracker/AggiungiActivity.kt
@@ -7,5 +7,8 @@ class AggiungiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aggiungi)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title = "Pulsante selezionato"
     }
 }
