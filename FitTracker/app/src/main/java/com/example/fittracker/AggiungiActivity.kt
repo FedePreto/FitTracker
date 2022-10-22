d8f737257d3651b8976a3506e77fb117b142ca98
S1092514 <s1092514@studenti.univpm.it>
2022-10-22 14:41:41 +0200

    aggiornamento

---
+++ FitTracker/app/src/main/java/com/example/fittracker/AggiungiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fittracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AggiungiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aggiungi)
+    }
+}
