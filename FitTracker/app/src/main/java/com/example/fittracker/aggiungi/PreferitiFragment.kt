1990bbc76b68941819832c01e6129371f4979131
S1092514 <s1092514@studenti.univpm.it>
2022-10-28 18:46:01 +0200

    layout delete

--- FitTracker/app/src/main/java/com/example/fittracker/aggiungi/PreferitiFragment.kt
+++ FitTracker/app/src/main/java/com/example/fittracker/aggiungi/PreferitiFragment.kt
@@ -1,11 +1,15 @@
 package com.example.fittracker.aggiungi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -84,6 +88,26 @@ class PreferitiFragment : Fragment() {
         })
     }*/
 
+    private fun openUpdateDeleteDialog(position: Int){
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.delete_layout, null)
+        val item = dialogLayout.findViewById<TextView>(R.id.tV_elimina)
+            item.text = "Vuoi eliminare $position dalla lista?"
+
+        with(builder){
+            setTitle("ELIMINARE")
+            setPositiveButton("Elimina"){dialog, which ->
+
+
+            }
+            setNegativeButton("Annulla"){ dialog, which ->
+                Log.d("Main", "Negative button clicked")
+            }
+            setView(dialogLayout)
+            show()
+        }
+    }
 
 
 
