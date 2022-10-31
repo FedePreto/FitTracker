a665497847950e612d8bbe778cd8183a599f3bda
S1092514 <s1092514@studenti.univpm.it>
2022-10-31 21:25:32 +0100

    draw.io

--- FitTracker/app/src/androidTest/java/com/example/fittracker/home/InizioActivityTest.kt
+++ FitTracker/app/src/androidTest/java/com/example/fittracker/home/InizioActivityTest.kt
@@ -6,7 +6,6 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -16,7 +15,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.fittracker.R
 import com.example.fittracker.autenticazione.InizioActivity
-import com.example.fittracker.autenticazione.LoginActivity
 import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Test
--- FitTracker/app/src/test/java/com/example/fittracker/FabbisognoTest.kt
+++ FitTracker/app/src/test/java/com/example/fittracker/FabbisognoTest.kt
@@ -14,7 +14,6 @@ class FabbisognoTest {
 
     // Test per vedere se il metodo calculateFabbisogno calcola correttamente le calorie giornaliere
 
-    //SUCCESSO
     @Test
     fun check_CorrectFabbisogno(){
         val fabbisogno = Utente()
@@ -22,15 +21,20 @@ class FabbisognoTest {
         Assert.assertEquals(3230, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
     }
 
-    //FALLIRE
     @Test
-    fun check_FakeFabbisogno(){
+    fun check_UncorrectFabbisogno1(){
         val fabbisogno = Utente()
         val utente = Utente("Alessandro","Rongoni","alerong@gmail.com",1.725,true,"Uomo","2000-05-18",183,76.0,"Calcio","Climatica")
-        Assert.assertEquals(2650, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
+        Assert.assertNotEquals(3229, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
     }
 
-    //SUCCESSO
+    @Test
+    fun check_UncorrectFabbisogno2(){
+        val fabbisogno = Utente()
+        val utente = Utente("Alessandro","Rongoni","alerong@gmail.com",1.725,true,"Uomo","2000-05-18",183,76.0,"Calcio","Climatica")
+        Assert.assertNotEquals(3231, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
+    }
+
     @Test
     fun check_LAFSedentarioFabbisogno(){
         val fabbisogno = Utente()
@@ -38,7 +42,6 @@ class FabbisognoTest {
         Assert.assertEquals(2247, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
     }
 
-    //SUCCESSO
     @Test
     fun check_DonnaFabbisogno(){
         val fabbisogno = Utente()
@@ -46,12 +49,11 @@ class FabbisognoTest {
         Assert.assertEquals(1239, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
     }
 
-    //FALLIRE
     @Test
     fun check_DonnaCambioEtaFabbisogno(){
         val fabbisogno = Utente()
         val utente = Utente("Martina","Rossi","martina.rossiOF.com",1.55,false,"Donna","1999-06-23",167,55.0,"","Climatica")
-        Assert.assertEquals(1239, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
+        Assert.assertNotEquals(1239, fabbisogno.calculateFabbisogno(utente.data_nascita,utente.sesso,utente.peso_attuale,utente.altezza,utente.LAF))
     }
 
 
